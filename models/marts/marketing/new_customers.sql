0982a0bafa0f706f0aa35f06069a91923cc0283d
Sreeram <snudurupati@outlook.com>
2025-10-22 15:25:00 -0700

    new demo branch with an empty model

--- models/marts/marketing/new_customers.sql
+++ models/marts/marketing/new_customers.sql
@@ -1,10 +1,1 @@
-SELECT
-  DATE_TRUNC('MONTH', FIRST_ORDER_DATE) AS month,
-  COUNT(customer_id) AS new_customers
-FROM {{ref("dim_customer")}}
-WHERE
-  DATE_PART('year', FIRST_ORDER_DATE) = DATE_PART('year', CURRENT_DATE) - 1
-GROUP BY
-  1
-ORDER BY
-  1 NULLS FIRST
+--write model code here
